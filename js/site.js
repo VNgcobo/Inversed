d5cce63fdfe48d739777e7de305a7a935764a3ea
VNgcobo <85413379+VNgcobo@users.noreply.github.com>
2021-08-19 19:23:28 +0200

    Build of Inversed Completed

--- js/site.js
+++ js/site.js
@@ -1,4 +1,42 @@
-//Call Hello World
-function helloWorld(){
-    alert("hello World");
+//Get the string from the page
+//controller function
+
+function getValue(){
+   document.getElementById("alert").classList.add("invisible");
+
+   let userString = document.getElementById("userString").value;
+
+   let revString = reverseString(userString);
+
+   displayString(revString);
+}
+
+
+//Reverse the string 
+//Logic function
+function reverseString(userString){
+    let revString = [];
+
+    //let name=Bobby
+    //name[0] = "B";
+    //name[4] = "y";
+    //last pos in an array = name.length-1
+
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+
+    return revString;
+}
+
+//Display message with the reversed string to the user
+//View Function
+function displayString(revString){
+    
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+
 }
