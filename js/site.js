791eca1da7f8187a36b0a9b1f68f5e370d46b4e1
VNgcobo <85413379+VNgcobo@users.noreply.github.com>
2021-09-20 17:59:34 +0200

    Detailed description + rebuild
    Updated code description in html page, and rebuilt the java page

--- js/site.js
+++ js/site.js
@@ -2,41 +2,41 @@
 //controller function
 
 function getValue(){
-   document.getElementById("alert").classList.add("invisible");
-
-   let userString = document.getElementById("userString").value;
-
-   let revString = reverseString(userString);
-
-   displayString(revString);
+    document.getElementById("alert").classList.add("invisible"); //why add an invisible class only to remove it later?
+ 
+    let userString = document.getElementById("userString").value;
+ 
+    let revString = reverseString(userString);
+ 
+    displayString(revString);
 }
-
-
+ 
+ 
 //Reverse the string 
 //Logic function
 function reverseString(userString){
     let revString = [];
-
-    //let name=Bobby
-    //name[0] = "B";
-    //name[4] = "y";
-    //last pos in an array = name.length-1
-
-    //reverse a string using a for loop
-    for (let index = userString.length - 1; index >= 0; index--) {
-        revString += userString[index];
+ 
+     //let name=Bobby
+     //name[0] = "B";
+     //name[4] = "y";
+     //last pos in an array = name.length-1
+ 
+     //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) { 
+         revString += userString[index]; //what does this mean? index on the left is added to the index on the right
     }
-
+ 
     return revString;
 }
-
+ 
 //Display message with the reversed string to the user
 //View Function
 function displayString(revString){
-    
-    //write to the page
-    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
-    //show the alert box
-    document.getElementById("alert").classList.remove("invisible");
-
+     
+     //write to the page
+     document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+     //show the alert box
+     document.getElementById("alert").classList.remove("invisible");
+ 
 }
